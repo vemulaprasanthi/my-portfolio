b9d7148e80eb3a63650f5222a7e3d743cda7c623
vemulaprasanthi <prasanthi.vemula@sasi.ac.in>
2025-10-26 16:43:14 +0530

    Updated education section timeline and styles

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
-// Contact form submission
+
 document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault(); // Prevent default form submission
-
-  // Initialize EmailJS with your public key
-  emailjs.init("Z1TzbAAUDqeL5k5sT"); // Replace with your EmailJS public key
-
-  // Send form using EmailJS
-  emailjs.sendForm("service_77zozkv", "template_b5u55ep", this) // Replace with your Template ID
+  e.preventDefault(); 
+  emailjs.init("Z1TzbAAUDqeL5k5sT"); 
+  emailjs.sendForm("service_77zozkv", "template_b5u55ep", this) 
     .then(() => {
-      // Success alert
       Swal.fire({
         icon: 'success',
         title: 'Message Sent!',
         text: 'Thank you! I will get back to you shortly.',
         confirmButtonColor: '#008075'
       });
-      this.reset(); // Reset form fields
+      this.reset(); 
     })
     .catch((error) => {
-      // Error alert
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
